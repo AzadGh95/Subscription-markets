39cb130d60b154a5493749c24d36d752b0244b5b
Azade Ghasemi <46772227+AzadGh95@users.noreply.github.com>
2022-12-28 20:52:18 +0330

    kernel

---
+++ app/Console/Commands/CheckSubscription.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class CheckSubscription extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'check:subscriptions';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+
+
+        return Command::SUCCESS;
+    }
+}
--- app/Services/Subscription.php
+++ app/Services/Subscription.php
@@ -18,4 +18,18 @@ class Subscription
         $statistic->status = $status;
         $statistic->save();
     }
+
+    public function Guzzle(string $url)
+    {
+        $client = new GuzzleHttp\Client();
+        $res = $client->request('GET', 'https://api.github.com/user', [
+            'auth' => ['user', 'pass']
+        ]);
+        echo $res->getStatusCode();
+        // "200"
+        echo $res->getHeader('content-type')[0];
+        // 'application/json; charset=utf8'
+        echo $res->getBody();
+        // {"type":"User"...'
+    }
 }
