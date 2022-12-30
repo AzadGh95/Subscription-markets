20101368b1afc198c57d71e8a06ed24d8567e7d5
Azade Ghasemi <46772227+AzadGh95@users.noreply.github.com>
2022-12-30 14:08:26 +0330

    Update CheckSubscriptionTest.php

--- tests/Feature/Console/Commands/CheckSubscriptionTest.php
+++ tests/Feature/Console/Commands/CheckSubscriptionTest.php
@@ -3,7 +3,6 @@
 namespace Tests\Feature\Console\Commands;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Support\Facades\Http;
 use Tests\TestCase;
 
 class CheckSubscriptionTest extends TestCase
@@ -15,67 +14,8 @@ class CheckSubscriptionTest extends TestCase
      *
      * @return void
      */
-    public function test_command_subscription_active()
+    public function test_command_subscription()
     {
-        $bodyGoogleplay = file_get_contents(base_path('tests/Feature/Helpers/googleplaydata_active.json'));
-        $apiGoogleplay = Http::fake([
-            'https//api.googleplay.co/*' => Http::response($bodyGoogleplay, 200),
-        ]);
-
-        $bodyAppleStore = file_get_contents(base_path('tests/Feature/Helpers/googleplaydata_active.json'));
-        $apiAppleStore = Http::fake([
-            'https//api.applestore.co/*' => Http::response($bodyAppleStore, 200),
-        ]);
-
-        $this->artisan('check:subscriptions', [
-            'applestore_api' => $apiAppleStore,
-            'googplay_api' => $apiGoogleplay,
-        ])->assertExitCode(0);
-    }
-
-    /**
-     * Test a console command.
-     *
-     * @return void
-     */
-    public function test_command_subscription_expired()
-    {
-        $bodyGoogleplay = file_get_contents(base_path('tests/Feature/Helpers/googleplaydata_expired.json'));
-        $apiGoogleplay = Http::fake([
-            'https//api.googleplay.co/*' => Http::response($bodyGoogleplay, 200),
-        ]);
-
-        $bodyAppleStore = file_get_contents(base_path('tests/Feature/Helpers/googleplaydata_expired.json'));
-        $apiAppleStore = Http::fake([
-            'https//api.applestore.co/*' => Http::response($bodyAppleStore, 200),
-        ]);
-
-        $this->artisan('check:subscriptions', [
-            'applestore_api' => $apiAppleStore,
-            'googplay_api' => $apiGoogleplay,
-        ])->assertExitCode(0);
-    }
-
-    /**
-     * Test a console command.
-     *
-     * @return void
-     */
-    public function test_command_subscription_failed()
-    {
-        $bodyGoogleplay = file_get_contents(base_path('tests/Feature/Helpers/googleplaydata_active.json'));
-        $apiGoogleplay = Http::fake([
-            'https//api.googleplay.co/*' => Http::response($bodyGoogleplay, 404),
-        ]);
-
-        $bodyAppleStore = file_get_contents(base_path('tests/Feature/Helpers/googleplaydata_active.json'));
-        $apiAppleStore = Http::fake([
-            'https//api.applestore.co/*' => Http::response($bodyAppleStore, 404),
-        ]);
-
-        $this->artisan('check:subscriptions', [
-            'applestore_api' => $apiAppleStore,
-            'googplay_api' => $apiGoogleplay,
-        ])->assertExitCode(0);
+        $this->artisan('check:subscriptions')->assertExitCode(0);
     }
 }
